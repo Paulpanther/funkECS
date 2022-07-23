860d49d3f5ebccbca039eb59332d55fbc78ce659
Tom Beckmann <tomjonabc@gmail.com>
2022-07-23 12:28:42 +0200

    wip

--- src/public/App.tsx
+++ src/public/App.tsx
@@ -1,5 +1,26 @@
 import React from "react";
+import Editor from "react-simple-code-editor";
+import Parser from "web-tree-sitter";
 
 export const App: React.VFC = () => {
-    return <div>Hello World</div>
+  const initializeParsing = async () => {
+    const parser = new Parser();
+    // await parser.init();
+  };
+
+  const [code, setCode] = React.useState("");
+  return (
+    <div>
+      <Editor
+        value={code}
+        onValueChange={(code) => setCode(code)}
+        highlight={(code) => code}
+        padding={10}
+        style={{
+          fontFamily: '"Fira code", "Fira Mono", monospace',
+          fontSize: 12,
+        }}
+      />
+    </div>
+  );
 };
