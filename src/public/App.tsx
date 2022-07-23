a202ad60ddabd9e06ec38ef38da8127428d88a7d
Tom Beckmann <tomjonabc@gmail.com>
2022-07-23 12:37:10 +0200

    cleanup ui codoe

--- src/public/App.tsx
+++ src/public/App.tsx
@@ -1,26 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Editor from "react-simple-code-editor";
 import Parser from "web-tree-sitter";
 
 export const App: React.VFC = () => {
-  const initializeParsing = async () => {
-    const parser = new Parser();
-    // await parser.init();
-  };
+  const [parser, setParser] = useState<Parser | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      await Parser.init();
+      const parser = new Parser();
+      parser.setLanguage(await Parser.Language.load("typescript"));
+      setParser(parser);
+    })();
+  });
 
   const [code, setCode] = React.useState("");
+
   return (
     <div>
-      <Editor
-        value={code}
-        onValueChange={(code) => setCode(code)}
-        highlight={(code) => code}
-        padding={10}
-        style={{
-          fontFamily: '"Fira code", "Fira Mono", monospace',
-          fontSize: 12,
-        }}
-      />
+      {parser ? (
+        <Editor
+          value={code}
+          onValueChange={(code) => setCode(code)}
+          highlight={(code) => code}
+          padding={10}
+          style={{
+            fontFamily: '"Fira code", "Fira Mono", monospace',
+            fontSize: 12,
+          }}
+        />
+      ) : (
+        "Loading..."
+      )}
     </div>
   );
 };
